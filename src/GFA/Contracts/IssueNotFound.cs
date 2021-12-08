4bbca87fd75e67921d3d8aa04c08fc7176f35690
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-12-08 11:13:27 +0800

    Remove Contracts There is no use to Contracts

--- src/GFA/Contracts/IssueNotFound.cs
+++
@@ -1,8 +0,0 @@
-namespace GFA.Contracts;
-
-public record IssueNotFound
-{
-    public long RepositoryId { get; init; }
-    public int Number { get; init; }
-    public string Title { get; init; } = "";
-}
